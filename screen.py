d0752fbea2b1930e54119584bb60a693bbd09f1a
pnkg99 <116551009+pnkg99@users.noreply.github.com>
2025-09-15 22:38:53 +0200

    Update screen.py

--- screen.py
+++ screen.py
@@ -28,7 +28,7 @@ class NFCPollingThread(QThread):
                     
                     # Emituj signal samo za nove kartice
                     if self.last_uid != uid_hex:
-                        self.card_detected.emit(bytes(uid_hex))
+                        self.card_detected.emit(bytes(uid))  # originalni UID u bytes
                         self.last_uid = uid_hex
                 else:
                     # Resetuj UID ako nema kartice
